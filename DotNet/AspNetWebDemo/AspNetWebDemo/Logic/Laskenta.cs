638be83eca600ef28fa4aa0f0c870a3e0ca98b94
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-21 13:55:12 +0200

    Lisätty ASP.NET-esimerkki.

---
+++ DotNet/AspNetWebDemo/AspNetWebDemo/Logic/Laskenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetWebDemo.Logic
+{
+    public class Laskenta
+    {
+        public int Summa(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ DotNet/AspNetWebDemo/AspNetWebDemoTests/Logic/LaskentaTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AspNetWebDemo.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetWebDemo.Logic.Tests
+{
+    [TestClass()]
+    public class LaskentaTests
+    {
+        [TestMethod()]
+        public void SummaTest()
+        {
+            int a = 100;
+            int b = 200;
+
+            Laskenta laskenta = new Laskenta();
+            int summa = laskenta.Summa(a, b);
+
+            int odotus = a + b;
+            Assert.AreEqual(odotus, summa);
+        }
+    }
+}
