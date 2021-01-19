a6f5f688acc75a6adae726a4f2e1d262e2df8121
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-19 09:18:26 +0200

    Aloitettu koodaamaan C#-esimerkkiä säätietojen hakuun.

--- DotNet/LokiinKirjoitus/LokiinKirjoitus/Program.cs
+++ DotNet/LokiinKirjoitus/LokiinKirjoitus/Program.cs
@@ -1,21 +1,42 @@
 ﻿using System;
-using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 namespace LokiinKirjoitus
 {
     class Program
     {
+        private static List<string> lokimerkinnät = new List<string>();
+
         static void Main(string[] args)
         {
             KirjoitaLokiin("Moi!");
+
+            /*
+            Console.ReadLine();
+
+            List<string> testi = new List<string>();
+            for (int i = 0; i < 50000; i++)
+            {
+                testi.Add("Testtestitesti123");
+            }
+
+            Console.WriteLine("Valmis.");
+            Console.ReadLine();
+            */
+
             KirjoitaLokiin("Sovelluksen suoritus päättyy.");
         }
 
+        /// <summary>
+        /// Kirjoittaa annetun viestin tekstitiedostoon päivämäärän kanssa.
+        /// </summary>
+        /// <param name="viesti">Haluttu viesti.</param>
         public static void KirjoitaLokiin(string viesti)
         {
             string kellonaika = DateTime.Now.ToString();
             string rivi = kellonaika + ": " + viesti + "\r\n";
+            lokimerkinnät.Add(rivi);
 
             const string TiedostoNimi = @"C:\Academy\Turku2020\GitTesti\DotNet\Loki.txt";
 
@@ -27,7 +48,8 @@ namespace LokiinKirjoitus
             loki.Close();
             */
 
-            using (FileStream loki = new FileStream(TiedostoNimi, FileMode.Append))
+            using (System.IO.FileStream loki =
+                new System.IO.FileStream(TiedostoNimi, System.IO.FileMode.Append))
             {
                 byte[] tavut = Encoding.UTF8.GetBytes(rivi);
                 loki.Write(tavut, 0, tavut.Length);
---
+++ DotNet/Säätiedot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Säätiedot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
