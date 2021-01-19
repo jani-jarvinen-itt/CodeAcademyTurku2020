715392a90bf577c8609cd8ced02b6af1960ddd76
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-19 15:33:50 +0200

    Lisätty LINQ-kysely suomalaisten asiakkaiden hakemista varten.

--- DotNet/EntityFrameworkTesti/EntityFrameworkTesti/Program.cs
+++ DotNet/EntityFrameworkTesti/EntityFrameworkTesti/Program.cs
@@ -1,5 +1,6 @@
 ﻿using EntityFrameworkTesti.Models;
 using System;
+using System.Linq;
 
 namespace EntityFrameworkTesti
 {
@@ -10,7 +11,24 @@ namespace EntityFrameworkTesti
             Console.WriteLine("Hello World!");
 
             NorthwindContext context = new NorthwindContext();
+
+            // suomalaiset asiakkaat, ratkaisu 1
+            /*
             foreach (Customer customer in context.Customers)
+            {
+                if (customer.Country == "Finland")
+                {
+                    Console.WriteLine(customer.CompanyName);
+                }
+            }
+            */
+
+            // suomalaiset asiakkaat, ratkaisu 2, LINQ-kysely
+            var suomalaiset = from c in context.Customers
+                              where c.Country == "Finland"
+                              select c;
+
+            foreach (Customer customer in suomalaiset)
             {
                 Console.WriteLine(customer.CompanyName);
             }
