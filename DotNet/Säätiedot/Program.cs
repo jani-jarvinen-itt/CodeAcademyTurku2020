4689d437ce5f21778bad5d3d2406458399828ab4
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-19 09:25:58 +0200

    Lisätty async-toiminallisuus sekä HttpClient-luokka.

--- DotNet/Säätiedot/Program.cs
+++ DotNet/Säätiedot/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Säätiedot
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Säätietojen haku alkaa.");
+
+            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+
+            Console.WriteLine("Sovelluksen suoritus päättyy.");
         }
     }
 }
