457ee9e447e0c9b0e47b5c160c8ca25ed10cef91
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-20 11:54:26 +0200

    Lisätty uusi sivu MVC-projektiin.

--- DotNet/AspNewMvcDemo/AspNewMvcDemo/Controllers/HomeController.cs
+++ DotNet/AspNewMvcDemo/AspNewMvcDemo/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AspNewMvcDemo.Controllers
             return View();
         }
 
+        public IActionResult UusiSivu()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
