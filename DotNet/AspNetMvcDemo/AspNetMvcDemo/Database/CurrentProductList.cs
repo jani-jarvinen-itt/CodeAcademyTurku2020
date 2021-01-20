af6c7d7fe1d9c0ba3ce9fb3dcc23a4bb7c8450b7
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-20 15:23:47 +0200

    Lisätty entity-malli osaksi ASP.NET-projektia.

---
+++ DotNet/AspNetMvcDemo/AspNetMvcDemo/Database/CurrentProductList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AspNetMvcDemo.Database
+{
+    public partial class CurrentProductList
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
